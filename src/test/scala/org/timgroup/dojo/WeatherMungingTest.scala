6c36b98af80fabc15b57e07ca64375cd20044691
Mike Cialowicz <mike.cialowicz@youdevise.com>
2012-05-31 12:58:22 -0400

    "empty" commit... previous commit isn't showing up in GitHub yet...

--- src/test/scala/org/timgroup/dojo/WeatherMungingTest.scala
+++ src/test/scala/org/timgroup/dojo/WeatherMungingTest.scala
@@ -4,7 +4,6 @@ import org.scalatest.FunSuite
 import org.scalatest.matchers.ShouldMatchers
 
 class WeatherMungingTest extends FunSuite with ShouldMatchers with ReadFile {
-
   test("Determines the team with the smallest spread between for and agains") {
     val dayWithSmallestTemperatureSpread = WeatherMunging(readFile("weather.dat")).dayWithTheSmallestTemperatureSpread
 
